a297841fabb6ebfba5ede9234e5c81865e99fddb
ZakHasMo <115020828+ZakHasMo@users.noreply.github.com>
2023-01-02 20:06:04 +0000

    Add files via upload

---
+++ optional_task.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+count = 0
+for x in range(0,num):
+    if num > 10:
+        print(num)
+        continue
+if num <= 10:
+    print("Sorry, too small")
+
+
+
+
